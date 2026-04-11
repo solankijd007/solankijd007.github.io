9030a10f9977403bf268395d66c586b019c3cc31
Jagdish Solanki <solankijd5182@gmail.com>
2026-04-11 15:46:03 +0530

    [Fix] :- bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/solankijd007',
+  basePath: isProd ? '/solankijd007' : '',
   images: {
     unoptimized: true,
   },
